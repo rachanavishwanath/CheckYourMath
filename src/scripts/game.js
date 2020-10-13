9bcd34978604b131c032c72583c1408a137d23fd
rachanavishwanath <rachana.gujrathi@gmail.com>
2020-10-12 19:53:37 -0700

    Added instructions for game

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -3,8 +3,9 @@ import Numbers from './numbers';
 const Util = require('./util');
 
 const NumColumns = 10;
-const DIM_X = 800;
+const DIM_X = [0, 80, 160, 240, 320, 400, 480, 560, 640, 720];
 const DIM_Y = 600;
+
 export default class Game {
   constructor(context) {
     this.bottomline = [];
@@ -12,22 +13,22 @@ export default class Game {
     this.bottomline = this.InitialLayer(context, NumColumns, this.bottomline);
   }
   InitialLayer(context, NumColumns, bottomline) {
-    debugger;
     let num;
     let posAtX = 0;
-    console.log(bottomline);
     for (let i = 0; i < NumColumns; i++) {
-        debugger
-      num = Math.round(Math.random() * 10);
+      num = Math.round(Math.random() * 10) % 10;
       const newNumber = new Numbers([posAtX, 474], num);
-      console.log(newNumber);
       newNumber.drawSquare(context);
       bottomline.push(newNumber);
       posAtX += 80;
     }
   }
 
-  randomPositionforInitialLayer() {}
+    randomPositionforFallingNumbers() {
+        const x = DIM_X[Math.round(Math.random() * 10) % 10];
+        const y = 0;
+        return [x, y];
+    }
 
   detectCollision() {}
   fallingNumber() {}
